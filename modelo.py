425f7cd0c9a74cd7b9802da71e7846396c9bee93
Vicente Couto Marczewski <vicentemarczewski@hotmail.com>
2021-07-21 19:23:34 -0300

    fim aula 1 parte 4

--- modelo.py
+++ modelo.py
@@ -1,3 +1,33 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
+        self.nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = 0 # este o usuário não definirá, será incrementado
+
+    def dar_like(self): # lembrar que só vale dentro da classe
+        self.likes += 1
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+
+vingadores.dar_like() #testando o incremento
+atlanta.dar_like()
+vingadores.dar_like()
+
+
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} minutos - '
+      f'Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - '
+      f'Likes: {atlanta.likes}')
 
